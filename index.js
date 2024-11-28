e96071ba3e0c4a8434588586be337cd7290b9a1a
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-28 21:16:45 +0800

    Add a middleware
    - This middleware can trigger an error or go to the next middleware or route handler
    - next callback function that has an argument will trigger an error
    - next callback function that has no argument, the string route/router or an empty string will not trigger an error

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log("I am triggered");
+	// If a string is provided (except for the string "route" and "router" or an empty string) as the argument for the next callback function, Express will treats it that ana error has ocuured and will skip the next middleware and route handlers
+	// It goes straight to the error handler middleware
+	// If an error handler middleware is not defined Express will trigger an error with the message provided. The argument is displayed on the page and it will appear on the console in red color
+	//  If nothing is provided as an argument, the next callback function will got to the next middleware or route handlers
+
+	// Comment one of the next callback function below to see the difference in behaviour
+	next(); // This will not trigger an error
+	//next("saya error"); // This will trigger an error
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
